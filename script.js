da11d94a7e4cd2fc8a15fec34fa64bcb43ff07ae
Epshtein Dmitry <letuver@yahoo.com>
2023-07-19 01:38:48 +0300

    first scripts

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = "";
+const screens = "";
+const screenPrice = 0;
+const rollback = true;
+const fullPrice = 0;
+const adaptive = 0;
+
+alert("Hello World!!!");
+
+console.log("Hello World!!!");
